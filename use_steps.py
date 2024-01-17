e0f64bad9084824a2ac5812a8c54c1b39d95d1a9
Arturo Gonzalez <arturoglz2004@gmail.com>
2024-01-17 10:37:25 -0600

    calculates epuck odometry using get_steps

--- use_steps.py
+++ use_steps.py
@@ -12,20 +12,17 @@ y = 0
 
 with open(csv_filename, mode='r') as csv_file:
     csv_reader = csv.reader(csv_file)
-    header = next(csv_reader)  # Skip header
-
-    positions_data = []  # List to store positions data
+    header = next(csv_reader)  
+    positions_data = [] 
 
     index = 2
     for row in csv_reader:
         
         left_steps, right_steps, left_encoder_prev, right_encoder_prev = map(int, row)
         
-        # Perform your equation or calculations here
         left_displacement = (left_steps - left_encoder_prev)
         right_displacement = (right_steps - right_encoder_prev)
         
-        # Calculate linear and angular displacement in meters and radians
         linear_distance = (left_displacement + right_displacement) * (2 * math.pi * wheel_radius) / (2 * 1000)
         angular_distance = (right_displacement - left_displacement) * (2 * math.pi) / (1000 * wheel_separation_distance) 
         '''
@@ -42,10 +39,8 @@ with open(csv_filename, mode='r') as csv_file:
 with open(output_csv_filename, mode='w', newline='') as output_csv_file:
     csv_writer = csv.writer(output_csv_file)
     
-    # Write header to the new CSV file
     csv_writer.writerow(['X', 'Y'])
 
-    # Write positions data to the new CSV file
     csv_writer.writerows(positions_data)
 
 print(f"Positions data saved to {output_csv_filename}")
