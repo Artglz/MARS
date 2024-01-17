0e42e7f97914c8c08f84124515a08bf988adf6c6
Arturo Gonzalez <arturoglz2004@gmail.com>
2024-01-17 10:37:01 -0600

    plots epuck odometry

--- plot_epuck.py
+++ plot_epuck.py
@@ -2,24 +2,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-# Load the CSV file into a DataFrame
-csv_file_path = r'C:\Users\artur\Downloads\epuck\output_positions.csv'
+csv_file_path = r'C:\Users\artur\Downloads\epuck_crazyflie\robot_trajectory.csv'
 df = pd.read_csv(csv_file_path)
 
-# Extract x and y values from the DataFrame
 x_values = df['X']
 y_values = df['Y']
 
-# Plot the x and y values
 plt.plot(x_values, y_values, marker='o', linestyle='-', color='b', label='Path')
 
-# Add labels and title
 plt.xlabel('X Axis Label')
 plt.ylabel('Y Axis Label')
 plt.title('Epuck Path')
 
-# Add a legend
 plt.legend()
 
-# Show the plot
 plt.show()
